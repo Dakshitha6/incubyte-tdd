4686751c225dee80885c78e8eb5c0d2df1112a88
Dakshitha KN <dakshitha.kn@tandemloop.in>
2025-09-11 16:37:26 +0530

    Test for custom delimeter * to multipy added

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,13 +1,20 @@
 function add(numbers) {
   if (numbers === "") return 0;
 
+  let isMultiply;
   let delimiter = /,|\n/;
   if (numbers.startsWith("//")) {
     const parts = numbers.split("\n");
-    delimiter = new RegExp(parts[0].slice(2));
+    isMultiply = parts[0].slice(2) == "*" ? true : false;
+    if (isMultiply) {
+      delimiter = new RegExp("\\*");
+    } else {
+      delimiter = new RegExp(parts[0].slice(2));
+    }
     numbers = parts[1];
   }
 
+  console.log("delimiter", delimiter);
   const values = numbers.split(delimiter).map(Number);
   const negativeNumbers = values.filter((v) => v < 0);
   if (negativeNumbers.length > 0) {
@@ -17,6 +24,11 @@ function add(numbers) {
   }
 
   let sum = 0;
+  if (isMultiply) {
+    let prod = 1;
+    for (let val of values) prod *= val;
+    return prod;
+  }
   for (let v of values) sum += v;
   return sum;
 }
